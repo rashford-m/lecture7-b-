105e8791d2f739a4d924f337f12a2033a55c76ff
Rashford <mikearowo@gmail.com>
2023-01-28 21:37:21 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  Axios.get("https://catfact.ninja/fact").then((res) => {
+    console.log(res.data);
+  });
   return (
     <div className="App">
-      
-    
+      <button>Generate Cat Fact</button>
+      <p></p>
     </div>
   );
 }
