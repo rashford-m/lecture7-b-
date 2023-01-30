1221ccdf8cd01c344b08c6c2375742a544e38459
Rashford <mikearowo@gmail.com>
2023-01-30 23:59:21 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [name, setName] = useState("");
-  const [predictAge, setPredictAge] = useState(null);
+  const [predictedAge, setPredictedAge] = useState(null);
   const fetchData = () => {
     Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
-      console.log(res.data);
+      setPredictedAge(res.data);
     });
   };
   return (
@@ -19,7 +19,9 @@ function App() {
         }}
       />
       <button onClick={fetchData}>Predict Age</button>
-      <h1>Predicted Age</h1>
+      <h1>Name: {predictedAge?.name}</h1>
+      <h1>Predicted Age: {predictedAge?.age}</h1>
+      <h1>Count: {predictedAge?.count}</h1>
     </div>
   );
 }
